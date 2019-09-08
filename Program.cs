33a997efcf22b76bb42d0a86e089945930b4648f
Dalton <dalton_akio@hotmail.com>
2019-09-08 16:24:24 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CompareString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] text = "teste".ToCharArray();
+            char[] text1 = "teste1".ToCharArray();
+            char[] text2= "test".ToCharArray();
+            char[] text3 = "testw".ToCharArray();
+            char[] text4 = "testwee".ToCharArray();
+            System.Console.WriteLine(Compare.VerifySimilarity(text, text1));
+            System.Console.WriteLine(Compare.VerifySimilarity(text, text2));
+            System.Console.WriteLine(Compare.VerifySimilarity(text, text3));
+            System.Console.WriteLine(Compare.VerifySimilarity(text, text4));
+        }
+    }
+}
